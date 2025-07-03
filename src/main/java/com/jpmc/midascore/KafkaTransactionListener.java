89b4a970b42a8d1d2b8ff2cb9ef51064c3b9561f
VioleGrace-Bam <yousifalrahmani2@gmail.com>
2025-07-03 23:52:54 +0400

    Implement Kafka listener for transaction processing

--- src/main/java/com/jpmc/midascore/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/KafkaTransactionListener.java
@@ -1,5 +1,17 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 public class KafkaTransactionListener {
     
+    @KafkaListener(
+        topics = "${general.kafka-topic}", 
+        groupId = "midas_core_group")
+    
+    
+        public void listen(Transaction transaction) {
+        // Process the transaction
+        System.out.println("Received transaction: " + transaction);
+    }
 }
