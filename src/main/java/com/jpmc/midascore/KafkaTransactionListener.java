1c00a525fe20f48ca98aaf3d757ca68940955ad2
VioleGrace-Bam <yousifalrahmani2@gmail.com>
2025-07-04 23:34:14 +0400

    Update application.yml and KafkaTransactionListener for Kafka configuration

--- src/main/java/com/jpmc/midascore/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/KafkaTransactionListener.java
@@ -3,11 +3,14 @@ package com.jpmc.midascore;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+@Component
+// This class listens to Kafka messages on the specified topic and processes transactions
 public class KafkaTransactionListener {
     
     @KafkaListener(
         topics = "${general.kafka-topic}", 
-        groupId = "midas_core_group")
+        groupId = "midas-core-group")
     
     
         public void listen(Transaction transaction) {
